a3beaa3b5f2119e3ed108d960b4def08fc5926a8
madi <madimaliaka@gmail.com>
2025-02-14 12:08:24 +0000

    move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,20 @@
 export const cart = [];
+
+export function addToCart (productId) {
+    let matchingItem;
+    
+    cart.forEach((cartItem) =>{
+       if (productId === item.product){
+         matchingItem= cartItem;
+       }
+    }) 
+    if (matchingItem) {
+      matchingItem += 1;
+    
+    } else{
+      cart.push({
+        productId: productId,
+        quantity: 1
+      })
+    }
+  }
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js'
 
 let productsHTML =' ';
@@ -63,36 +63,22 @@ products.forEach((product) => {
 document.querySelector('.js-products-grid').innerHTML= productsHTML
 
 
-document.querySelectorAll('.js-add-to-cart')     //adding the cart made interative and responsive
-.forEach((button) => {
- button.addEventListener('click', () => {
-  const productId= button.dataset.productId
-
-
-let matchingItem;
-
-cart.forEach((item) =>{
-   if (productId === item.product){
-     matchingItem= item;
-   }
-}) 
-if (matchingItem) {
-  matchingItem += 1;
-
-} else{
-  cart.push({
-    productId: productId,
-    quantity: 1
-  })
-}
-
-//make the cartquantity interactive
+function updateCartQuantity(){
+ //make the cartquantity interactive and update the cart
 let cartQuantity=0
 
-cart.forEach((item) => {
-   cartQuantity += item.quantity;
+cart.forEach((cartItem) => {
+   cartQuantity += cartItem.quantity;
 });
 document.querySelector('.js-cart-quantity')
 .innerHTML=cartQuantity
-})
+}
+
+document.querySelectorAll('.js-add-to-cart')     //adding the cart made interative and responsive on the web page using DOM
+  .forEach((button) => {
+   button.addEventListener('click', () => {
+    const productId= button.dataset.productId
+    addToCart(productId);
+     updateCartQuantity()
+  })
 })
