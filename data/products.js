5c3337db84a106bb60c00ade1cf1fed7a7e57f8d
madi <madimaliaka@gmail.com>
2025-02-23 21:25:12 +0000

    load from backend using caall back function

--- data/products.js
+++ data/products.js
@@ -58,11 +58,7 @@ const object2={
   b : this.a
 }*/
 
-function logThis(){
-  console.log(this)
-}
-logThis()
-logThis.call('hello')
+
 
 //arrrow function don't change the value of this 
 /*const object3={
@@ -72,6 +68,30 @@ logThis.call('hello')
 }
 object3.method()*/
 
+//load product from backend
+export let products=[]
+export function loadProducts(fun){
+  const xhr = new XMLHttpRequest()
+
+  xhr.addEventListener('load', () => {
+ products= JSON.parse(xhr.response).map((productDetails) => {
+  if(productDetails.type === "clothing"){
+    return new Clothing(productDetails)
+}
+  return new Product(productDetails)
+})
+console.log('load products')
+
+fun();
+})
+  xhr.open('GET', 'https://supersimplebackend.dev/products')
+  xhr.send()
+
+}
+
+
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -737,4 +757,4 @@ export const products = [
 }
   return new Product(productDetails)
 })
-
+*/
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,12 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 // import '../data/cart-class.js';
-import '../data/backend-practice.js'
+//import '../data/backend-practice.js'
+import { loadProducts } from "../data/products.js";
 
-renderOrderSummary(); 
-renderPaymentSummary()
+
+//load product from backend by using call back function
+loadProducts(() => {
+    renderOrderSummary(); 
+    renderPaymentSummary()
+ });
